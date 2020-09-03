8b6c2e130a673f5de13564bb533d6383f4a035d9
benjads <web+git@sihota.me>
2020-09-02 23:23:46 -0700

    Initial commit

---
+++ bwr-wp-toolkit.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: BWR Toolkit
+Plugin URI: https://www.bensihota.com
+Description: Big West Rotaract Utilities; please leave enabled
+Version: 1.0
+Author: Benjamin Sihota
+Author URI: https://www.bensihota.com
+License: GPLv3
+*/
